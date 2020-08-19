d54549d420218cfdaf71b83cdc61f493f155065d
Crystal Knight <crystalknightcodes@gmail.com>
2020-08-19 00:03:02 -0400

    Added About and Life VC files, and wired up the outlets and actions

---
+++ Project/Game of Life/Game of Life/AboutViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AboutViewController.swift
+//  Game of Life
+//
+//  Created by Christy Hicks on 8/18/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet var moreButton: UIButton!
+    @IBOutlet var moreTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project/Game of Life/Game of Life/LifeViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LifeViewController.swift
+//  Game of Life
+//
+//  Created by Christy Hicks on 8/18/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class LifeViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var fastForwardButton: UIButton!
+    @IBOutlet var generationLabel: UILabel!
+    
+    // MARK: - View
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+    @IBAction func preset1Button(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func preset2Button(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func preset3Button(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func preset4Button(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func playPauseToggle(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func fastForwardToggle(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stopButton(_ sender: UIButton) {
+        
+    }
+}
