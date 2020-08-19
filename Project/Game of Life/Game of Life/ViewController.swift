65d259212438dcf364df9c3841ca6dc3343db10d
Crystal Knight <CrystalKnightCodes@gmail.com>
2020-08-18 22:30:44 -0400

    Initial Commit

---
+++ Project/Game of Life/Game of Life/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game of Life
+//
+//  Created by Christy Hicks on 8/18/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
