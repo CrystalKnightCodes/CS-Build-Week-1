0fb612c82d200e9c586bd75f447ac0f9b0963bbf
Crystal Knight <crystalknightcodes@gmail.com>
2020-08-19 20:58:35 -0400

    Created Cell file and class

---
+++ Project/Game of Life/Game of Life/Cell.swift
@@ -0,0 +1,23 @@
+//
+//  Cell.swift
+//  Game of Life
+//
+//  Created by Christy Hicks on 8/19/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import Foundation
+
+class Cell {
+    let id: Int
+    let row: Int
+    let column: Int
+    var isAlive: Bool
+    
+    init(id: Int, row: Int, column: Int, isAlive: Bool = false) {
+        self.id = id
+        self.row = row
+        self.column = column
+        self.isAlive = isAlive
+    }
+}
