8931b25e68d0571260babb75c8f8279bf46b0aac
Crystal Knight <crystalknightcodes@gmail.com>
2020-08-19 21:29:05 -0400

    Created cell grid in CellView

--- Project/Game of Life/Game of Life/CellView.swift
+++ Project/Game of Life/Game of Life/CellView.swift
@@ -9,13 +9,51 @@
 import UIKit
 
 class CellView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - Properties
+    private var path = UIBezierPath()
+    
+    fileprivate var heightAndWidth: CGFloat {
+        return 25
     }
-    */
-
+    
+    fileprivate var gridWidth: CGFloat {
+        return bounds.width/CGFloat(heightAndWidth)
+    }
+    
+    fileprivate var gridHeight: CGFloat {
+        return bounds.height/CGFloat(heightAndWidth)
+    }
+    
+    fileprivate var gridCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    // MARK: - Methods
+    fileprivate func drawGrid() {
+        path = UIBezierPath()
+        path.lineWidth = 2.0
+        
+        for index in 0...Int(heightAndWidth) {
+            let start = CGPoint(x: CGFloat(index) * gridWidth, y: 0)
+            let end = CGPoint(x: CGFloat(index) * gridWidth, y:bounds.height)
+            path.move(to: start)
+            path.addLine(to: end)
+        }
+        
+        for index in 0...Int(heightAndWidth) {
+            let start = CGPoint(x: 0, y: CGFloat(index) * gridHeight)
+            let end = CGPoint(x: bounds.width, y: CGFloat(index) * gridHeight)
+            path.move(to: start)
+            path.addLine(to: end)
+        }
+        
+        path.close()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        drawGrid()
+        
+        UIColor.purple.setStroke()
+        path.stroke()
+    }
 }
