188df6bb6ce635b93ea341c9851d533ba745ee28
Crystal Knight <crystalknightcodes@gmail.com>
2020-08-24 21:28:51 -0400

    Changed Grid's didSet property to only activate during when calculating next generation. Renamed methods and variables for clarity.  Changed neighbors method to return only the unique neighbors. Tweaked methods so generations would calculate properly.

--- Project/Game of Life/Game of Life/CellController.swift
+++ Project/Game of Life/Game of Life/CellController.swift
@@ -13,22 +13,25 @@ class CellController {
     // MARK: - Properties
     var grid: Grid {
         didSet {
-            updateFutureCellsBuffer { futureCells in
-                self.futureCellsBuffer = Grid(cells: futureCells)
+            if !isInitialSetup {
+                updateFutureGrid { currentCells in
+                    self.futureGrid = Grid(cells: currentCells)
+                }
             }
         }
     }
     
-    var futureCellsBuffer: Grid? {
+    var futureGrid: Grid? {
         didSet {
             guard updateReady else { return }
             updateReady = false
-            updateCells()
+            updateGrid()
         }
     }
     
     var generations = 0
     var updateReady = false
+    var isInitialSetup = false
     
     // MARK: - Initializers
     init(grid: Grid? = nil) {
@@ -40,17 +43,17 @@ class CellController {
     }
     
     // MARK: - Methods
-    func updateCells() {
-        guard let futureCells = futureCellsBuffer else {
+    func updateGrid() {
+        guard let currentGrid = futureGrid else {
             updateReady = true
             return
         }
         
-        grid = futureCells
+        grid = currentGrid
         generations += 1
         
-        updateFutureCellsBuffer { futureCells in
-            self.futureCellsBuffer = Grid(cells: futureCells)
+        updateFutureGrid { currentCells in
+            self.futureGrid = Grid(cells: currentCells)
         }
     }
     
@@ -60,20 +63,23 @@ class CellController {
     // Any live cell with more than three live neighbors will die.
     // Any dead cell with exactly 3 live neighbors will become a live cell.
     
-    func updateFutureCellsBuffer(completion: @escaping ([Cell]) -> Void) {
+    func updateFutureGrid(completion: @escaping ([Cell]) -> Void) {
         let currentCells = grid.cells
         var futureCells = currentCells
-        futureCellsBuffer = nil
+        futureGrid = nil
         
-        DispatchQueue.global(qos: .background).async {
+        DispatchQueue.global(qos: .background).sync {
             for (index, currentCell) in currentCells.enumerated() {
                 switch self.numberOfAliveNeighbors(for: currentCell) {
-                // Live cells with 2-3 live neighbors will live.
-                case 2...3 where currentCell.isAlive == true:
-                    break
+                    
                 // Dead cells with exactly 3 live neighbors become alive.
                 case 3 where currentCell.isAlive == false:
                     futureCells[index].isAlive = true
+
+                // Live cells with 2-3 live neighbors will continue to live.
+                case 2...3 where currentCell.isAlive == true:
+                    futureCells[index].isAlive = true
+                    
                 // Everything else dies.
                 default:
                     futureCells[index].isAlive = false
@@ -85,192 +91,247 @@ class CellController {
         }
     }
 
-    
     // Finding the neighbors
     func neighbors(for cell: Cell) -> [Cell] {
-        var neighbors = [Cell?]()
+        var neighbors = [Cell]()
         
-        for row in -1...1 {
-            for column in -1...1 {
-                guard !(column == 0 && row == 0) else { continue }
-                let neighbor = grid.findCell(row: Int(cell.row) + row, column: Int(cell.column) + column)
-                neighbors.append(neighbor)
+        for y in -1...1 {
+            for x in -1...1 {
+                guard !(y == 0 && x == 0) else { continue }
+                let neighbor = grid.findCell(row: cell.row + x, column: cell.column + y)
+                if neighbor != nil {
+                    neighbors.append(neighbor!)
+                }
             }
         }
-        return neighbors.compactMap { $0 }
+        
+        let uniqueNeighbors = Array(Set(neighbors))
+        return uniqueNeighbors
     }
     
     func numberOfAliveNeighbors(for cell: Cell) -> Int {
-        neighbors(for: cell).filter { $0.isAlive == true }.count
+        return neighbors(for: cell).filter { $0.isAlive == true }.count
     }
     
     
     // Setting the cell state
-    func activateCell(cellRow: CGFloat, cellColumn: CGFloat) {
-        let newCell = Cell(row: cellRow, column: cellColumn, isAlive: true)
-        futureCellsBuffer?.cells.remove(at: newCell.id - 1)
-        futureCellsBuffer?.cells.insert(newCell, at: newCell.id - 1)
+    func activateCell(cellRow: Int, cellColumn: Int) {
+        futureGrid?.toggleCell(row: cellRow, column: cellColumn, isAlive: true)
     }
     
-    func deactivateCell(cellRow: CGFloat, cellColumn: CGFloat) {
-        let newCell = Cell(row: cellRow, column: cellColumn, isAlive: false)
-        futureCellsBuffer?.cells.remove(at: newCell.id - 1)
-        futureCellsBuffer?.cells.insert(newCell, at: newCell.id - 1)
+    func deactivateCell(cellRow: Int, cellColumn: Int) {
+        futureGrid?.toggleCell(row: cellRow, column: cellColumn, isAlive: false)
     }
     
     // Preset configurations
     func createToad() {
+        futureGrid = grid
         activateCell(cellRow: 3, cellColumn: 22)
         activateCell(cellRow: 3, cellColumn: 23)
-        activateCell(cellRow: 3, cellColumn: 24)
+        activateCell(cellRow: 3, cellColumn: 21)
         activateCell(cellRow: 4, cellColumn: 21)
         activateCell(cellRow: 4, cellColumn: 22)
-        activateCell(cellRow: 4, cellColumn: 23)
-        updateCells()
+        activateCell(cellRow: 4, cellColumn: 20)
+        isInitialSetup = true
+        grid = futureGrid!
+        isInitialSetup = false
+    }
+    
+    func destroyToad() {
+        futureGrid = grid
+        deactivateCell(cellRow: 3, cellColumn: 22)
+        deactivateCell(cellRow: 3, cellColumn: 23)
+        deactivateCell(cellRow: 3, cellColumn: 21)
+        deactivateCell(cellRow: 4, cellColumn: 21)
+        deactivateCell(cellRow: 4, cellColumn: 22)
+        deactivateCell(cellRow: 4, cellColumn: 20)
+        isInitialSetup = true
+        grid = futureGrid!
+        isInitialSetup = false
+    }
+    
+    func createGlider() {
+        futureGrid = grid
+        activateCell(cellRow: 3, cellColumn: 14)
+        activateCell(cellRow: 4, cellColumn: 12)
+        activateCell(cellRow: 4, cellColumn: 14)
+        activateCell(cellRow: 5, cellColumn: 13)
+        activateCell(cellRow: 5, cellColumn: 14)
+        isInitialSetup = true
+        grid = futureGrid!
+        isInitialSetup = false
+    }
+    
+    func destroyGlider() {
+        futureGrid = grid
+        deactivateCell(cellRow: 3, cellColumn: 14)
+        deactivateCell(cellRow: 4, cellColumn: 12)
+        deactivateCell(cellRow: 4, cellColumn: 14)
+        deactivateCell(cellRow: 5, cellColumn: 13)
+        deactivateCell(cellRow: 5, cellColumn: 14)
+        isInitialSetup = true
+        grid = futureGrid!
+        isInitialSetup = false
     }
     
     func createPulsar() {
-        activateCell(cellRow: 9, cellColumn: 10)
-        activateCell(cellRow: 9, cellColumn: 11)
-        activateCell(cellRow: 9, cellColumn: 15)
-        activateCell(cellRow: 9, cellColumn: 16)
-        activateCell(cellRow: 10, cellColumn: 9)
+        futureGrid = grid
+        activateCell(cellRow: 7, cellColumn: 9)
+        activateCell(cellRow: 7, cellColumn: 10)
+        activateCell(cellRow: 7, cellColumn: 11)
+        activateCell(cellRow: 7, cellColumn: 15)
+        activateCell(cellRow: 7, cellColumn: 16)
+        activateCell(cellRow: 7, cellColumn: 17)
+        activateCell(cellRow: 9, cellColumn: 7)
+        activateCell(cellRow: 9, cellColumn: 12)
+        activateCell(cellRow: 9, cellColumn: 14)
+        activateCell(cellRow: 9, cellColumn: 19)
+        activateCell(cellRow: 10, cellColumn: 7)
         activateCell(cellRow: 10, cellColumn: 12)
         activateCell(cellRow: 10, cellColumn: 14)
-        activateCell(cellRow: 10, cellColumn: 17)
-        activateCell(cellRow: 11, cellColumn: 9)
+        activateCell(cellRow: 10, cellColumn: 19)
+        activateCell(cellRow: 11, cellColumn: 7)
         activateCell(cellRow: 11, cellColumn: 12)
         activateCell(cellRow: 11, cellColumn: 14)
-        activateCell(cellRow: 11, cellColumn: 17)
+        activateCell(cellRow: 11, cellColumn: 19)
+        activateCell(cellRow: 12, cellColumn: 9)
         activateCell(cellRow: 12, cellColumn: 10)
         activateCell(cellRow: 12, cellColumn: 11)
         activateCell(cellRow: 12, cellColumn: 15)
         activateCell(cellRow: 12, cellColumn: 16)
+        activateCell(cellRow: 12, cellColumn: 17)
+        activateCell(cellRow: 14, cellColumn: 9)
         activateCell(cellRow: 14, cellColumn: 10)
         activateCell(cellRow: 14, cellColumn: 11)
         activateCell(cellRow: 14, cellColumn: 15)
         activateCell(cellRow: 14, cellColumn: 16)
-        activateCell(cellRow: 15, cellColumn: 9)
+        activateCell(cellRow: 14, cellColumn: 17)
+        activateCell(cellRow: 15, cellColumn: 7)
         activateCell(cellRow: 15, cellColumn: 12)
         activateCell(cellRow: 15, cellColumn: 14)
-        activateCell(cellRow: 15, cellColumn: 17)
-        activateCell(cellRow: 16, cellColumn: 9)
+        activateCell(cellRow: 15, cellColumn: 19)
+        activateCell(cellRow: 16, cellColumn: 7)
         activateCell(cellRow: 16, cellColumn: 12)
         activateCell(cellRow: 16, cellColumn: 14)
-        activateCell(cellRow: 16, cellColumn: 17)
-        activateCell(cellRow: 17, cellColumn: 10)
-        activateCell(cellRow: 17, cellColumn: 11)
-        activateCell(cellRow: 17, cellColumn: 15)
-        activateCell(cellRow: 17, cellColumn: 16)
-        updateCells()
-    }
-    
-    func createSpaceship() {
-        activateCell(cellRow: 2, cellColumn: 6)
-        activateCell(cellRow: 2, cellColumn: 7)
-        activateCell(cellRow: 3, cellColumn: 2)
-        activateCell(cellRow: 3, cellColumn: 3)
-        activateCell(cellRow: 3, cellColumn: 4)
-        activateCell(cellRow: 3, cellColumn: 5)
-        activateCell(cellRow: 3, cellColumn: 7)
-        activateCell(cellRow: 3, cellColumn: 8)
-        activateCell(cellRow: 4, cellColumn: 2)
-        activateCell(cellRow: 4, cellColumn: 3)
-        activateCell(cellRow: 4, cellColumn: 4)
-        activateCell(cellRow: 4, cellColumn: 5)
-        activateCell(cellRow: 4, cellColumn: 6)
-        activateCell(cellRow: 4, cellColumn: 7)
-        activateCell(cellRow: 5, cellColumn: 3)
-        activateCell(cellRow: 5, cellColumn: 4)
-        activateCell(cellRow: 5, cellColumn: 5)
-        activateCell(cellRow: 5, cellColumn: 6)
-        updateCells()
-    }
-    
-    func createGlider() {
-        activateCell(cellRow: 22, cellColumn: 24)
-        activateCell(cellRow: 23, cellColumn: 22)
-        activateCell(cellRow: 23, cellColumn: 24)
-        activateCell(cellRow: 24, cellColumn: 23)
-        activateCell(cellRow: 24, cellColumn: 24)
-        updateCells()
-    }
-    
-    func destroyToad() {
-        deactivateCell(cellRow: 3, cellColumn: 22)
-        deactivateCell(cellRow: 3, cellColumn: 23)
-        deactivateCell(cellRow: 3, cellColumn: 24)
-        deactivateCell(cellRow: 4, cellColumn: 21)
-        deactivateCell(cellRow: 4, cellColumn: 22)
-        deactivateCell(cellRow: 4, cellColumn: 23)
-        updateCells()
+        activateCell(cellRow: 16, cellColumn: 19)
+        activateCell(cellRow: 17, cellColumn: 7)
+        activateCell(cellRow: 17, cellColumn: 12)
+        activateCell(cellRow: 17, cellColumn: 14)
+        activateCell(cellRow: 17, cellColumn: 19)
+        activateCell(cellRow: 19, cellColumn: 9)
+        activateCell(cellRow: 19, cellColumn: 10)
+        activateCell(cellRow: 19, cellColumn: 11)
+        activateCell(cellRow: 19, cellColumn: 15)
+        activateCell(cellRow: 19, cellColumn: 16)
+        activateCell(cellRow: 19, cellColumn: 17)
+        isInitialSetup = true
+        grid = futureGrid!
+        isInitialSetup = false
     }
     
     func destroyPulsar() {
-        deactivateCell(cellRow: 9, cellColumn: 10)
-        deactivateCell(cellRow: 9, cellColumn: 11)
-        deactivateCell(cellRow: 9, cellColumn: 15)
-        deactivateCell(cellRow: 9, cellColumn: 16)
-        deactivateCell(cellRow: 10, cellColumn: 9)
+        futureGrid = grid
+        deactivateCell(cellRow: 7, cellColumn: 9)
+        deactivateCell(cellRow: 7, cellColumn: 10)
+        deactivateCell(cellRow: 7, cellColumn: 11)
+        deactivateCell(cellRow: 7, cellColumn: 15)
+        deactivateCell(cellRow: 7, cellColumn: 16)
+        deactivateCell(cellRow: 7, cellColumn: 17)
+        deactivateCell(cellRow: 9, cellColumn: 7)
+        deactivateCell(cellRow: 9, cellColumn: 12)
+        deactivateCell(cellRow: 9, cellColumn: 14)
+        deactivateCell(cellRow: 9, cellColumn: 19)
+        deactivateCell(cellRow: 10, cellColumn: 7)
         deactivateCell(cellRow: 10, cellColumn: 12)
         deactivateCell(cellRow: 10, cellColumn: 14)
-        deactivateCell(cellRow: 10, cellColumn: 17)
-        deactivateCell(cellRow: 11, cellColumn: 9)
+        deactivateCell(cellRow: 10, cellColumn: 19)
+        deactivateCell(cellRow: 11, cellColumn: 7)
         deactivateCell(cellRow: 11, cellColumn: 12)
         deactivateCell(cellRow: 11, cellColumn: 14)
-        deactivateCell(cellRow: 11, cellColumn: 17)
+        deactivateCell(cellRow: 11, cellColumn: 19)
+        deactivateCell(cellRow: 12, cellColumn: 9)
         deactivateCell(cellRow: 12, cellColumn: 10)
         deactivateCell(cellRow: 12, cellColumn: 11)
         deactivateCell(cellRow: 12, cellColumn: 15)
         deactivateCell(cellRow: 12, cellColumn: 16)
+        deactivateCell(cellRow: 12, cellColumn: 17)
+        deactivateCell(cellRow: 14, cellColumn: 9)
         deactivateCell(cellRow: 14, cellColumn: 10)
         deactivateCell(cellRow: 14, cellColumn: 11)
         deactivateCell(cellRow: 14, cellColumn: 15)
         deactivateCell(cellRow: 14, cellColumn: 16)
-        deactivateCell(cellRow: 15, cellColumn: 9)
+        deactivateCell(cellRow: 14, cellColumn: 17)
+        deactivateCell(cellRow: 15, cellColumn: 7)
         deactivateCell(cellRow: 15, cellColumn: 12)
         deactivateCell(cellRow: 15, cellColumn: 14)
-        deactivateCell(cellRow: 15, cellColumn: 17)
-        deactivateCell(cellRow: 16, cellColumn: 9)
+        deactivateCell(cellRow: 15, cellColumn: 19)
+        deactivateCell(cellRow: 16, cellColumn: 7)
         deactivateCell(cellRow: 16, cellColumn: 12)
         deactivateCell(cellRow: 16, cellColumn: 14)
-        deactivateCell(cellRow: 16, cellColumn: 17)
-        deactivateCell(cellRow: 17, cellColumn: 10)
-        deactivateCell(cellRow: 17, cellColumn: 11)
-        deactivateCell(cellRow: 17, cellColumn: 15)
-        deactivateCell(cellRow: 17, cellColumn: 16)
-        updateCells()
+        deactivateCell(cellRow: 16, cellColumn: 19)
+        deactivateCell(cellRow: 17, cellColumn: 7)
+        deactivateCell(cellRow: 17, cellColumn: 12)
+        deactivateCell(cellRow: 17, cellColumn: 14)
+        deactivateCell(cellRow: 17, cellColumn: 19)
+        deactivateCell(cellRow: 19, cellColumn: 9)
+        deactivateCell(cellRow: 19, cellColumn: 10)
+        deactivateCell(cellRow: 19, cellColumn: 11)
+        deactivateCell(cellRow: 19, cellColumn: 15)
+        deactivateCell(cellRow: 19, cellColumn: 16)
+        deactivateCell(cellRow: 19, cellColumn: 17)
+        isInitialSetup = true
+        grid = futureGrid!
+        isInitialSetup = false
+    }
+    
+    func createSpaceship() {
+        futureGrid = grid
+        activateCell(cellRow: 3, cellColumn: 7)
+        activateCell(cellRow: 3, cellColumn: 8)
+        activateCell(cellRow: 4, cellColumn: 3)
+        activateCell(cellRow: 4, cellColumn: 4)
+        activateCell(cellRow: 4, cellColumn: 5)
+        activateCell(cellRow: 4, cellColumn: 6)
+        activateCell(cellRow: 4, cellColumn: 8)
+        activateCell(cellRow: 4, cellColumn: 9)
+        activateCell(cellRow: 5, cellColumn: 3)
+        activateCell(cellRow: 5, cellColumn: 4)
+        activateCell(cellRow: 5, cellColumn: 5)
+        activateCell(cellRow: 5, cellColumn: 6)
+        activateCell(cellRow: 5, cellColumn: 7)
+        activateCell(cellRow: 5, cellColumn: 8)
+        activateCell(cellRow: 6, cellColumn: 4)
+        activateCell(cellRow: 6, cellColumn: 5)
+        activateCell(cellRow: 6, cellColumn: 6)
+        activateCell(cellRow: 6, cellColumn: 7)
+        isInitialSetup = true
+        grid = futureGrid!
+        isInitialSetup = false
     }
     
     func destroySpaceship() {
-        deactivateCell(cellRow: 2, cellColumn: 6)
-        deactivateCell(cellRow: 2, cellColumn: 7)
-        deactivateCell(cellRow: 3, cellColumn: 2)
-        deactivateCell(cellRow: 3, cellColumn: 3)
-        deactivateCell(cellRow: 3, cellColumn: 4)
-        deactivateCell(cellRow: 3, cellColumn: 5)
+        futureGrid = grid
         deactivateCell(cellRow: 3, cellColumn: 7)
         deactivateCell(cellRow: 3, cellColumn: 8)
-        deactivateCell(cellRow: 4, cellColumn: 2)
         deactivateCell(cellRow: 4, cellColumn: 3)
         deactivateCell(cellRow: 4, cellColumn: 4)
         deactivateCell(cellRow: 4, cellColumn: 5)
         deactivateCell(cellRow: 4, cellColumn: 6)
-        deactivateCell(cellRow: 4, cellColumn: 7)
+        deactivateCell(cellRow: 4, cellColumn: 8)
+        deactivateCell(cellRow: 4, cellColumn: 9)
         deactivateCell(cellRow: 5, cellColumn: 3)
         deactivateCell(cellRow: 5, cellColumn: 4)
         deactivateCell(cellRow: 5, cellColumn: 5)
         deactivateCell(cellRow: 5, cellColumn: 6)
-        updateCells()
-    }
-    
-    func destroyGlider() {
-        deactivateCell(cellRow: 22, cellColumn: 24)
-        deactivateCell(cellRow: 23, cellColumn: 22)
-        deactivateCell(cellRow: 23, cellColumn: 24)
-        deactivateCell(cellRow: 24, cellColumn: 23)
-        deactivateCell(cellRow: 24, cellColumn: 24)
-        updateCells()
+        deactivateCell(cellRow: 5, cellColumn: 7)
+        deactivateCell(cellRow: 5, cellColumn: 8)
+        deactivateCell(cellRow: 6, cellColumn: 4)
+        deactivateCell(cellRow: 6, cellColumn: 5)
+        deactivateCell(cellRow: 6, cellColumn: 6)
+        deactivateCell(cellRow: 6, cellColumn: 7)
+        isInitialSetup = true
+        grid = futureGrid!
+        isInitialSetup = false
     }
 }
 
